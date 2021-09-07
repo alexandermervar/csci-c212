cc7399899bcef48e34d61270cc7e71066658bb9e
alexandermervar <alexandermervar@gmail.com>
2021-09-07 19:04:54 -0400

    Added name and username to later files

--- Labs/lab02/mervar_lob02_4-5/src/com/alexandermervar/Lab2Vowels.java
+++ Labs/lab02/mervar_lob02_4-5/src/com/alexandermervar/Lab2Vowels.java
@@ -1,3 +1,16 @@
+//  Lab 2
+//
+//  Released:  1/20/20
+//
+//  @Author  Alexander Mervar amervar
+//  Last Edited: 9.7.2021
+//
+//
+//  Directions:  Implement the following methods
+//
+//
+//////////////////////////////////////////////////////////////////////////////////
+
 package com.alexandermervar;
 
 public class Lab2Vowels {
