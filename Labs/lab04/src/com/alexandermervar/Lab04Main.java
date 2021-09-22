0022e49da582b816347a6b4e292110880fe3e300
alexandermervar <alexandermervar@gmail.com>
2021-09-21 23:03:04 -0400

    Lab04 Exercise 3 patternMenu() Completed - 9.21.2021

--- Labs/lab04/src/com/alexandermervar/Lab04Main.java
+++ Labs/lab04/src/com/alexandermervar/Lab04Main.java
@@ -8,15 +8,16 @@ public class Lab04Main {
 
         // Exercise 1
         // Call function secretNumber()
+
         Lab2Exercises.secretNumber();
 
         // Exercise 2
         // Call function exponentCalculator(int base, int exponent)
-        System.out.println("Should be 9: " + Lab2Exercises.exponentCalculator(3,2));
-        System.out.println("Should be 16: " + Lab2Exercises.exponentCalculator(4,2));
-        System.out.println("Should be 3125: " + Lab2Exercises.exponentCalculator(5,5));
-        System.out.println("Should be 125: " + Lab2Exercises.exponentCalculator(5,3));
-        System.out.println("Should be an Error: " + Lab2Exercises.exponentCalculator(5,-3));
+        System.out.println("Should be 9: " + Lab2Exercises.exponentCalculator(3, 2));
+        System.out.println("Should be 16: " + Lab2Exercises.exponentCalculator(4, 2));
+        System.out.println("Should be 3125: " + Lab2Exercises.exponentCalculator(5, 5));
+        System.out.println("Should be 125: " + Lab2Exercises.exponentCalculator(5, 3));
+        // System.out.println("Should be an Error: " + Lab2Exercises.exponentCalculator(5,-3));
 
         // Exercise 3
         // Call function patternMenu()
--- Labs/lab04/src/com/alexandermervar/Lab2Exercises.java
+++ Labs/lab04/src/com/alexandermervar/Lab2Exercises.java
@@ -56,7 +56,7 @@ public class Lab2Exercises {
 
         boolean run = true;
 
-        while(run?) {
+        while(run) {
             System.out.println("Please select the pattern that you wish to create:");
             System.out.println("A. Arrow Pointing Left");
             System.out.println("B. Right Angle Triangle (Pointing Down and to the Left)");
@@ -67,31 +67,165 @@ public class Lab2Exercises {
             String user = sc.nextLine();
             Random ran = new Random();
 
-            //TODO: Define size
-            int size = ran.nextInt()
+            int size = ran.nextInt(8) + 3;
 
             // ERROR
-            if(user != "A" || user != "B" || user != "C" || user != "D" || user != "E") {
+            if((int) user.charAt(0) < 65 || user.charAt(0) > 69) {
                 throw new IllegalArgumentException("Error: Invalid Menu Selection");
             }
 
-            if(user == "A") {
+            if(user.charAt(0) == 'A') {
+                // Top Half
+                int numStars = 1;
+                for(int i=0; i < size; i++) {
+                    //Spaces
+                    for(int j = 0; j < size - numStars; j++) {
+                        System.out.print(" ");
+                    }
+                    // Stars
+                    for(int j = 0; j < numStars; j++) {
+                        System.out.print("*");
+                    }
+                    numStars++;
+                    System.out.println();
+                }
 
+                // Bottom Half
+                numStars = size - 1;
+                for(int i=1; i < size; i++) {
+                    //Spaces
+                    for(int j = 0; j < size - numStars; j++) {
+                        System.out.print(" ");
+                    }
+                    // Stars
+                    for(int j = 0; j < numStars; j++) {
+                        System.out.print("*");
+                    }
+                    numStars--;
+                    System.out.println();
+                }
             }
 
-            else if(user == "B") {
+            else if(user.charAt(0) == 'B') {
+                String legend = String.valueOf(size);
+                // System.out.println("Legend: " + legend);
+                int lineLength = 1;
 
+                // Height
+                for(int i = 0; i < size; i++) {
+                    for(int j = 0; j < lineLength; j++) {
+                        if(j == lineLength - 1) {
+                            System.out.println(legend);
+                        }
+                        else if(j%2==0){
+                            System.out.print(legend);
+                        }
+                        else {
+                            System.out.print(" ");
+                        }
+                    }
+                    lineLength += 2;
+                }
             }
 
-            else if(user == "C") {
+            else if(user.charAt(0) == 'C') {
+                //System.out.println("Size: " + size);
 
+                int lineLength = (size * 2) - 1;
+
+                // Height
+                for(int i = 0; i < size; i++) {
+                    // Top Line
+                    if (i == 0) {
+                        for(int j = 0; j < lineLength; j++) {
+                            if(j == lineLength - 1) {
+                                System.out.println("*");
+                            }
+                            else if(j%2==0){
+                                System.out.print("*");
+                            }
+                            else {
+                                System.out.print(" ");
+                            }
+                        }
+                    }
+                    // Bottom Line
+                    else if (i == size - 1) {
+                        for(int j = 0; j < lineLength; j++) {
+                            if(j == lineLength - 1) {
+                                System.out.println("*");
+                            }
+                            else if(j%2==0){
+                                System.out.print("*");
+                            }
+                            else {
+                                System.out.print(" ");
+                            }
+                        }
+                    }
+                    // Middle
+                    else {
+                        for(int j = 0; j < lineLength; j++) {
+                            if(j == lineLength - 1) {
+                                System.out.println("*");
+                            }
+                            else if(j == 0){
+                                System.out.print("*");
+                            }
+                            else {
+                                System.out.print(" ");
+                            }
+                        }
+                    }
+                }
             }
 
-            else if(user == "D") {
+            else if(user.charAt(0) == 'D') {
+                int k = 0;
+                for (int i = 1; i <= size; i++)
+                {
 
+                    for (int j = i; j < size; j++) {
+                        System.out.print(" ");
+                    }
+
+                    while (k != (2 * i - 1)) {
+                        if (k == 0 || k == 2 * i - 2)
+                            System.out.print("*");
+                        else
+                            System.out.print(" ");
+                        k++;
+                    }
+
+                    k = 0;
+
+                    System.out.println();
+                }
+
+                for (int i = size - 1; i >= 1; i--)
+                {
+                    for (int j = size; j > i; j--) {
+                        System.out.print(" ");
+                    }
+
+                    System.out.print("*");
+
+                    for(int j = 1; j < (i - 1) * 2; j++){
+                        System.out.print(" ");
+                    }
+
+                    if (i == 1)
+                    {
+                        System.out.println();
+                    }
+                    else
+                    {
+                        System.out.println("*");
+                    }
+                }
             }
 
-            else if(user == "E") {
+            else if(user.charAt(0) == 'E') {
                 run = false;
             }
         }
