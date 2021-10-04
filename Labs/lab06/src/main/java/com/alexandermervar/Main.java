9acf477413743c6a07ef6b9ac75b2afb851c4405
alexandermervar <alexandermervar@gmail.com>
2021-10-04 09:31:01 -0400

    Lab06 Started and Officialy Switched IDE to Visual Studio Code

---
+++ Labs/lab06/src/main/java/com/alexandermervar/Main.java
@@ -0,0 +1,8 @@
+package com.alexandermervar;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+
+        }
+}
---
+++ Labs/lab06/src/test/java/com/alexandermervar/MainTest.java
@@ -0,0 +1,15 @@
+package com.alexandermervar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    
+    /*
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+    */
+}
