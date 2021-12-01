14e47e256a400e7a7e3588de61ad7125c0ebcccc
alexandermervar <alexandermervar@gmail.com>
2021-12-01 18:17:41 -0500

    Finished Queues and Priority Queues for Lab11

--- Labs/lab11/src/main/java/com/alexandermervar/QueuesandPriorityQueues/AmusementParkExample.java
+++ Labs/lab11/src/main/java/com/alexandermervar/QueuesandPriorityQueues/AmusementParkExample.java
@@ -1,7 +1,52 @@
 package com.alexandermervar.QueuesandPriorityQueues;
 
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class AmusementParkExample {
     public static void main(String[] args) {
-        // TODO
+        PriorityQueue<Customer> screamerLine = new PriorityQueue<>();
+        Ride priorityRide1 = new Ride("FastPass - Screamer", 4, screamerLine);
+        screamerLine.add(new Customer("Alexander", true));
+        screamerLine.add(new Customer("Jennifer", false));
+        screamerLine.add(new Customer("Perry", false));
+        screamerLine.add(new Customer("Haley", true));
+        screamerLine.add(new Customer("Samantha", false));
+        screamerLine.add(new Customer("Sophia", true));
+        screamerLine.add(new Customer("Sophie", false));
+        screamerLine.add(new Customer("Steve", false));
+        screamerLine.add(new Customer("Chris", false));
+        screamerLine.add(new Customer("Martin", true));
+
+
+        Queue<Customer> rollerCoasterLine = new LinkedList<>();
+        Ride noPriorityRide2 = new Ride("Roller Coaster", 3, rollerCoasterLine);
+        rollerCoasterLine.add(new Customer("Alexander", true));
+        rollerCoasterLine.add(new Customer("Jennifer", false));
+        rollerCoasterLine.add(new Customer("Perry", false));
+        rollerCoasterLine.add(new Customer("Haley", true));
+        rollerCoasterLine.add(new Customer("Samantha", false));
+        rollerCoasterLine.add(new Customer("Sophia", true));
+        rollerCoasterLine.add(new Customer("Sophie", false));
+        rollerCoasterLine.add(new Customer("Steve", false));
+        rollerCoasterLine.add(new Customer("Chris", false));
+        rollerCoasterLine.add(new Customer("Martin", true));
+
+        while (priorityRide1.getPassengers().size() > 0 || noPriorityRide2.getPassengers().size() > 0) {
+            System.out.println("------------------------------------------------------");
+            System.out.println("Running Rides:");
+            System.out.println("");
+            if (priorityRide1.getPassengers().size() > 0) {
+                System.out.println("Priority Ride: " + priorityRide1.getName());
+                priorityRide1.run();
+                System.out.println();
+            }
+            if (noPriorityRide2.getPassengers().size() > 0) {
+                System.out.println("No Priority Ride: " + noPriorityRide2.getName());
+                noPriorityRide2.run();
+            }
+            System.out.println("");
+        }
     }
 }
--- Labs/lab11/src/main/java/com/alexandermervar/QueuesandPriorityQueues/Ride.java
+++ Labs/lab11/src/main/java/com/alexandermervar/QueuesandPriorityQueues/Ride.java
@@ -1,13 +1,13 @@
 package com.alexandermervar.QueuesandPriorityQueues;
 
-import java.util.AbstractQueue;
+import java.util.Queue;
 
 public class Ride {
     
     // A ride class with instance variables for name, capacity, and a Queue of customers/passengers
     private String name;
     private int capacity;
-    private AbstractQueue<Customer> passengers;
+    private Queue<Customer> passengers;
 
     // Setters and getters
     public String getName() {
@@ -22,18 +22,19 @@ public class Ride {
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
-    public AbstractQueue<Customer> getPassengers() {
+    public Queue<Customer> getPassengers() {
         return passengers;
     }
-    public void setPassengers(AbstractQueue<Customer> passengers) {
+    public void setPassengers(Queue<Customer> passengers) {
         this.passengers = passengers;
     }
 
     // Constructor for the Ride class
-    public Ride(String name, int capacity, AbstractQueue<Customer> passengers) {
+    public Ride(String name, int capacity, Queue<Customer> passengers) {
         this.name = name;
         this.capacity = capacity;
         this.passengers = passengers;
+
     }
 
     // A run() function that removes people from the queue, up until the ride capacity is reached
@@ -42,18 +43,19 @@ public class Ride {
     public void run() {
         int count = 0;
         while (count < capacity && passengers.size() > 0) {
-            Customer customer = passengers.poll();
+            Customer customer = passengers.peek();
             if (customer.isFastPass()) {
                 System.out.println("Fastpass: " + customer.getName());
             } else {
                 System.out.println("Regular: " + customer.getName());
             }
+            passengers.remove();
             count++;
         }
     }
 
     // An addToQueue() function that can add a ArrayList of customers to the ride's queue
-    public void addToQueue(AbstractQueue<Customer> customers) {
+    public void addToQueue(Queue<Customer> customers) {
         for (Customer customer : customers) {
             passengers.add(customer);
         }
